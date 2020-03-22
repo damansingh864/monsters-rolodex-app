69d9432d82f42bb797df4e6eaa95e3dae37423be
Damanvir Singh <damanveer.singh@hoolime.co>
2020-03-22 13:58:24 +0530

    Updates

---
+++ precache-manifest.53d58f55cc618c93a0e1c94848e80f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cd8636f4b801dde37fcf69dff2a67f",
+    "url": "/monsters-rolodex-app/index.html"
+  },
+  {
+    "revision": "3bbcd56f801695a8fb5a",
+    "url": "/monsters-rolodex-app/static/css/main.6ad14a7b.chunk.css"
+  },
+  {
+    "revision": "c110a050b9fde1d70b31",
+    "url": "/monsters-rolodex-app/static/js/2.afa117bc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex-app/static/js/2.afa117bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbcd56f801695a8fb5a",
+    "url": "/monsters-rolodex-app/static/js/main.737df213.chunk.js"
+  },
+  {
+    "revision": "ea1761de136aba8c671f",
+    "url": "/monsters-rolodex-app/static/js/runtime-main.17870564.js"
+  }
+]);
